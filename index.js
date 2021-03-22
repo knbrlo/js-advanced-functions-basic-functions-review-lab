a45906c738d9f42fb39532632e8390e52bdd4876
Ken Barlow <kenbrlow@gmail.com>
2021-03-21 19:51:14 -0600

    commit tree c2d8377d0ab0b1bf0480bb6ab088d2da61c35898

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(paramOne="*") {
+    return function(paramTwo="special") {
+        return `You are ${paramOne}${paramTwo}${paramOne}!`
+    }
+}
+
+let Calculator = {
+    add: function(a, b) { return a + b },
+    subtract: function(a, b) { return a - b },
+    multiply: function(a, b) { return a * b },
+    divide: function(a, b) { return a / b }
+}
+
+function actionApplyer(start, functionsArray) {
+    if (functionsArray.length == 0) {
+        return start;
+    }
+
+    functionsArray.map(func => {
+      start = func(start)
+    })
+
+    return start;
+}
